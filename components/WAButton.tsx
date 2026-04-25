87338cf6950b4df05cfb77f4b1f5a2e165445697
toodiaz <j.antonio.diaz.luna@gmail.com>
2026-04-25 01:28:29 -0600

    feat: add components/WAButton.tsx

---
+++ components/WAButton.tsx
@@ -0,0 +1,8 @@
+export default function WAButton() {
+  return (
+    <a href="https://wa.me/521135759862943271" target="_blank"
+      className="fixed bottom-6 right-6 z-50 bg-green-500 text-white rounded-full p-4 shadow-lg hover:bg-green-600 text-2xl">
+      💬
+    </a>
+  )
+}
